4dadcc0b04bff2960893e27c608115ae0687327d
Jose Calvo <195166539+josecv92@users.noreply.github.com>
2026-02-25 01:42:20 +0100

    Arreglado endpoint usuarios y franja

--- backend/app/Models/Ausencia.php
+++ backend/app/Models/Ausencia.php
@@ -22,6 +22,6 @@ class Ausencia extends Model
     }
 
     public function user() {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'usuario_id');
     }
 }
